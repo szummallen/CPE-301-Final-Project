87a171e826879f84cf3c5887feb2ae086d273b8e
szummallen <szummallen@nevada.unr.edu>
2023-05-09 16:27:58 -0700

    Fixed the LiquidCrystal library functions, now onto the DHT11

--- FinalProject_301/FinalProject_301.ino
+++ FinalProject_301/FinalProject_301.ino
@@ -1,7 +1,5 @@
-#include <Adafruit_MCP23008.h>
-#include <Adafruit_LiquidCrystal.h>
+#include <LiquidCrystal.h>
 #include <DHT.h>
-#include <DHT_U.h>
 #include <Wire.h>
 #include <RTClib.h>
 #include <Stepper.h>
@@ -10,9 +8,6 @@
 const char* empty_str = "        ";
 enum STATE{OFF, IDL, ERR, RUN};
 STATE state_cur = IDL;
-
-#define setOutput(port, pin) (port) |= (1 << (pin))
-#define setInput(port, pin) (port) &= ~(1 << (pin))
 
 #define LED_yel_reg PORTC
 #define LED_yel_pin 0
@@ -47,34 +42,29 @@ STATE state_cur = IDL;
 #define pumpPowerPin_pin 3
 
 #define DHT11_PIN 3
+#define DHTTYPE DHT11
 
 #define disableButton_reg PORTC
 #define disableButton_pin 6
 
 LiquidCrystal lcd(&p_rs_reg, &p_en_reg, &p_d4_reg, &p_d5_reg, &p_d6_reg, &p_d7_reg);
-
 Stepper stepper(200, 8, 10, 9, 11);
-
 DHT dht(DHT11_PIN, DHT11);
-
 RTC_DS3231 rtc;
-
 int water_sensor_pin = A0;
 volatile bool water_level_high = false;
-
 int fan_power_pin1 = A1;
 int fan_power_pin2 = A2;
 int pump_power_pin = A3;
 int fan_speed = 0;
 int pump_speed = 0;
-
 int stepper_speed = 100;
-
 int disable_button_pin = 4;
 volatile bool disable_button_pressed = false;
-
 unsigned long previousTime = 0;
 const long interval = 1000;
+float temp = temperature;
+float humi = humidity;
 
 void setup(){
   Serial.begin(9600);
@@ -101,10 +91,10 @@ void setup(){
 }
 
 void setOutput(uint8_t port, int pin){
-  port |= (1 << (pin));
+  port |= (1 << pin);
 }
 void setInput(uint8_t port, int pin){
-  port &= ~(1 << (pin));
+  port &= ~(1 << pin);
 }
 
 void updateWaterLevel(){
@@ -118,7 +108,7 @@ void updateWaterLevel(){
   }
 }
 
-void readDHT11(float &temperature, float &humidity){
+void readDHT11(float temperature, float humidity){
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
 }
@@ -177,54 +167,38 @@ void loop() {
   water_level /= 16;
   if (water_level < 500){
     digitalWrite(waterSensorPower_pin, HIGH);
-    lcd_set_cursor(0, 0);
-    lcd_write_string("Water Level LOW");
-    lcd_set_cursor(0, 1);
-    lcd_write_string("Water Pump ON");
+    lcd.setCursor(0, 0);
+    lcd.print("Water Level LOW");
+    lcd.setCursor(0, 1);
+    lcd.print("Water Pump ON");
     analogWrite(pumpPowerPin_pin, 255);
   } else{
     digitalWrite(waterSensorPower_pin, LOW);
-    lcd_set_cursor(0, 0);
-    lcd_write_string("Water Level NOMINAL");
-    lcd_set_cursor(0, 1);
-    lcd_write_string("Water Pump OFF");
+    lcd.setCursor(0, 0);
+    lcd.print("Water Level NOMINAL");
+    lcd.setCursor(0, 1);
+    lcd.print("Water Pump OFF");
     analogWrite(pumpPowerPin_pin, 0);
   }
 
 
-  int chk = DHT.read11(DHT11_PIN);
+  int chk = readDHT11(DHT11_PIN);
   if (chk == DHTLIB_OK){
-    float temp = DHT.temperature;
-    float humi = DHT.humidity;
-    lcd_set_cursor(13, 0);
-    lcd_write_string("Temp:");
-    lcd_set_cursor(18, 0);
-    lcd_write_float(temp, 2);
-    lcd_write_string("C");
-    lcd_set_cursor(13, 1);
-    lcd_write_string("Humidity:");
-    lcd_set_cursor(22, 1);
-    lcd_write_float(humi, 2);
-    lcd_write_string("%");
+    
+    lcd.setCursor(13, 0);
+    lcd.print("Temp:");
+    lcd.setCursor(18, 0);
+    lcd.setCursor(temp, 2);
+    lcd.print("C");
+    lcd.setCursor(13, 1);
+    lcd.print("Humidity:");
+    lcd.setCursor(22, 1);
+    lcd.print(humi, 2);
+    lcd.print("%");
   } else{
-    lcd_set_cursor(13, 0);
-    lcd_write_string("Temp: --.- C");
-    lcd_set_cursor(13, 1);
-    lcd_write_string("Humidity: --.- %");
-  }
-
-  if(digitalRead(disableButton) == LOW){
-    lcd_set_cursor(0, 2);
-    lcd_write_string("Vent Control OFF");
-  } else {
-    int pot_value = analogRead(A4);
-    int steps = map(pot_value, 0, 1023, -MAX_STEPS, MAX_STEPS);
-    stepper.step(steps);
-    lcd_set_cursor(0, 2);
-    lcd_write_string("Vent Control ON");
-    lcd_set_cursor(0, 3);
-    lcd_write_string("Direction:");
-    lcd_set_cursor(11, 3);
-    lcd_write_string(steps >= 0 ? "FWD" : "REV");
+    lcd.setCursor(13, 0);
+    lcd.print("Temp: --.- C");
+    lcd.setCursor(13, 1);
+    lcd.print("Humidity: --.- %");
   }
 }

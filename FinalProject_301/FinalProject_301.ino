24c26994f2e72ef0969d1bece89372af20667740
szummallen <szummallen@nevada.unr.edu>
2023-05-09 14:58:58 -0700

    Very close to finishing, need to clean up and get Adafruit add on that works

--- FinalProject_301/FinalProject_301.ino
+++ FinalProject_301/FinalProject_301.ino
@@ -1,62 +1,61 @@
-#include <LiquidCrystal.h>
+#include <Adafruit_MCP23008.h>
+#include <Adafruit_LiquidCrystal.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Wire.h>
 #include <RTClib.h>
 #include <Stepper.h>
+#include <stdint.h>
 
 const char* empty_str = "        ";
 enum STATE{OFF, IDL, ERR, RUN};
 STATE state_cur = IDL;
-#define setOutput(pin) pinMode(pin, OUTPUT)
-#define setInput(pin) pinMode(pin, INPUT_PULLUP)
 
-#define LED_yel 12
-#define LED_grn 3
-#define LED_red 4
-#define LED_blue 5
+#define setOutput(port, pin) (port) |= (1 << (pin))
+#define setInput(port, pin) (port) &= ~(1 << (pin))
 
-#define p_rs 6
-#define p_en 7
-#define p_d4 8
-#define p_d5 9
-#define p_d6 10
-#define p_d7 11
+#define LED_yel_reg PORTC
+#define LED_yel_pin 0
+#define LED_grn_reg PORTC
+#define LED_grn_pin 1
+#define LED_red_reg PORTC
+#define LED_red_pin 2
+#define LED_blue_reg PORTC
+#define LED_blue_pin 3
 
-#define waterSensorPower 12
+#define p_rs_reg PORTB
+#define p_rs_pin 0
+#define p_en_reg PORTB
+#define p_en_pin 1
+#define p_d4_reg PORTB
+#define p_d4_pin 2
+#define p_d5_reg PORTB
+#define p_d5_pin 3
+#define p_d6_reg PORTD
+#define p_d6_pin 4
+#define p_d7_reg PORTD
+#define p_d7_pin 5
 
-#define fanPowerPin1 A0
-#define fanPowerPin2 A1
-#define pumpPowerPin A2
+#define waterSensorPower_reg PORTD
+#define waterSensorPower_pin 6
 
-#define disableButton A3
+#define fanPowerPin1_reg PORTC
+#define fanPowerPin1_pin 5
+#define fanPowerPin2_reg PORTC
+#define fanPowerPin2_pin 4
+#define pumpPowerPin_reg PORTC
+#define pumpPowerPin_pin 3
 
-void setOutput(int pin){ pinMode(pin, OUTPUT);}
-void setInput(int pin){ pinMode(pin, INPUT_PULLUP);}
+#define DHT11_PIN 3
 
-setOutput(LED_yel);
-setOutput(LED_grn);
-setOutput(LED_red);
-setOutput(LED_blue);
+#define disableButton_reg PORTC
+#define disableButton_pin 6
 
-setOutput(p_rs);
-setOutput(p_en);
-setOutput(p_d4);
-setOutput(p_d5);
-setOutput(p_d6);
-setOutput(p_d7);
-
-setOutput(fanPowerPin1);
-setOutput(fanPowerPin2);
-setOutput(pumpPowerPin);
-
-setInput(disableButton);
-
-LiquidCrystal lcd(p_rs, p_en, p_d4, p_d5, p_d6, p_d7);
+LiquidCrystal lcd(&p_rs_reg, &p_en_reg, &p_d4_reg, &p_d5_reg, &p_d6_reg, &p_d7_reg);
 
 Stepper stepper(200, 8, 10, 9, 11);
 
-DHT dht(2, DHT11);
+DHT dht(DHT11_PIN, DHT11);
 
 RTC_DS3231 rtc;
 
@@ -74,40 +73,48 @@ int stepper_speed = 100;
 int disable_button_pin = 4;
 volatile bool disable_button_pressed = false;
 
+unsigned long previousTime = 0;
+const long interval = 1000;
+
 void setup(){
   Serial.begin(9600);
   
-  setOutput(LED_yel);
-  setOutput(LED_grn);
-  setOutput(LED_red);
-  setOutput(LED_blue);
+  setOutput(LED_yel_reg, LED_yel_pin);
+  setOutput(LED_grn_reg, LED_grn_pin);
+  setOutput(LED_red_reg, LED_red_pin);
+  setOutput(LED_blue_reg, LED_blue_pin);
 
-  setOutput(p_rs);
-  setOutput(p_en);
-  setOutput(p_d4);
-  setOutput(p_d5);
-  setOutput(p_d6);
-  setOutput(p_d7);
+  setOutput(p_rs_reg, p_rs_pin);
+  setOutput(p_en_reg, p_en_pin);
+  setOutput(p_d4_reg, p_d4_pin);
+  setOutput(p_d5_reg, p_d5_pin);
+  setOutput(p_d6_reg, p_d6_pin);
+  setOutput(p_d7_reg, p_d7_pin);
 
-  setOutput(fanPowerPin1);
-  setOutput(fanPowerPin2);
-  setOutput(pumpPowerPin);
+  setOutput(fanPowerPin1_reg, fanPowerPin1_pin);
+  setOutput(fanPowerPin2_reg, fanPowerPin2_pin);
+  setOutput(pumpPowerPin_reg, pump_power_pin);
 
-  setInput(disableButton);
+  setInput(disableButton_reg, disable_button_pin);
 
-  digitalWrite(waterSensorPower, HIGH);
+  digitalWrite(waterSensorPower_reg, HIGH);
 }
 
-
+void setOutput(uint8_t port, int pin){
+  port |= (1 << (pin));
+}
+void setInput(uint8_t port, int pin){
+  port &= ~(1 << (pin));
+}
 
 void updateWaterLevel(){
   int water_level = analogRead(water_sensor_pin);
   if(water_level > 500){
     water_level_high = true;
-    digitalWrite(waterSensorPower, LOW);
+    PORTD &= ~(1 << waterSensorPower_pin);
   } else{
     water_level_high = false;
-    digitalWrite(waterSensorPower, HIGH);
+    PORTD |= (1 << waterSensorPower_pin);
   }
 }
 
@@ -128,35 +135,60 @@ void updateFanAndPump(float temperature, float humidity){
     pump_speed = 0;
   }
   
-  analogWrite(fan_power_pin1, fan_speed);
-  analogWrite(fan_power_pin2, fan_speed);
+  PORTB = fan_speed;
   analogWrite(pump_power_pin, pump_speed);
 
-  digitalWrite(LED_red, fan_speed > 0 || pump_speed > 0);
-  digitalWrite(LED_grn, fan_speed > 0 && pump_speed > 0);
-  digitalWrite(LED_yel, fan_speed == 0 && pump_speed > 0);
-  digitalWrite(LED_blue, fan_speed == 0 && pump_speed == 0);
+  if(fan_speed > 0 || pump_speed > 0){
+    PORTC |= (1 << LED_red_pin);
+  } else{
+    PORTC &= ~(1 << LED_red_pin);
+  }
+  
+  if(fan_speed > 0 && pump_speed > 0){
+    PORTC |= (1 << LED_grn_pin);
+  } else {
+    PORTC &= ~(1 << LED_grn_pin);
+  }
+
+  if(fan_speed == 0 && pump_speed > 0){
+    PORTC |= (1 << LED_yel_pin);
+  } else {
+    PORTC &= ~(1 << LED_yel_pin);
+  }
+
+  if(fan_speed == 0 && pump_speed == 0){
+    PORTC |= (1 << LED_blue_pin);
+  } else{
+    PORTC &= ~(1 << LED_blue_pin);
+  }
+}  
+
+
 
 void loop() {
+  if (millis() - previousTime >= interval){
+    previousTime = millis();
+  }
+
   uint16_t water_level = 0;
   for(int i = 0; i < 16; i++){
     water_level += analogRead(A5);
   }
   water_level /= 16;
-  if (water_level < WATER_LEV_THRESHOLD){
-    digitalWrite(waterSensorPower, HIGH);
+  if (water_level < 500){
+    digitalWrite(waterSensorPower_pin, HIGH);
     lcd_set_cursor(0, 0);
     lcd_write_string("Water Level LOW");
     lcd_set_cursor(0, 1);
     lcd_write_string("Water Pump ON");
-    analogWrite(pumpPowerPin, 255);
+    analogWrite(pumpPowerPin_pin, 255);
   } else{
-    digitalWrite(waterSensorPower, LOW);
+    digitalWrite(waterSensorPower_pin, LOW);
     lcd_set_cursor(0, 0);
     lcd_write_string("Water Level NOMINAL");
     lcd_set_cursor(0, 1);
     lcd_write_string("Water Pump OFF");
-    analogWrite(pumpPowerPin, 0);
+    analogWrite(pumpPowerPin_pin, 0);
   }
 
 
@@ -172,13 +204,13 @@ void loop() {
     lcd_set_cursor(13, 1);
     lcd_write_string("Humidity:");
     lcd_set_cursor(22, 1);
-    lcd_write_float(hum, 2);
+    lcd_write_float(humi, 2);
     lcd_write_string("%");
   } else{
     lcd_set_cursor(13, 0);
     lcd_write_string("Temp: --.- C");
     lcd_set_cursor(13, 1);
-    lcd_write_string("Humidity: --.- %")
+    lcd_write_string("Humidity: --.- %");
   }
 
   if(digitalRead(disableButton) == LOW){
@@ -192,9 +224,7 @@ void loop() {
     lcd_write_string("Vent Control ON");
     lcd_set_cursor(0, 3);
     lcd_write_string("Direction:");
-    lcd_set_cursor(11, 0);
+    lcd_set_cursor(11, 3);
     lcd_write_string(steps >= 0 ? "FWD" : "REV");
-  }
-  delay(1000);
   }
 }
